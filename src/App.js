c9a37e423e36e7f078e8397b1d6df4b28802876a
svinerus <svinerus@gmail.com>
2022-11-14 22:15:22 +0200

    Rework working with contract addresses

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
 import {useWeb3React} from '@web3-react/core';
-import {useEffect, useRef, useState} from 'react';
-import {
-  createBridgeContract,
-  ConfiguredInjectedConnector,
-  defaultContractAddress,
-  getAdresses
-} from './utils/contracts';
+import {useEffect, useState} from 'react';
+import {ConfiguredInjectedConnector, createBridgeContract, getContractAddresses} from './utils/contracts';
 import Multisig from "./multisig";
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+
   const {activate, account, library, chainId} = useWeb3React();
+  const [contractsList, setContractsList] = useState([]);
+  const [contractAddress, setContractAddress] = useState(queryParams.get('contract'));
+  const [contract, setContract] = useState(null);
 
-  const [addresses, setAddresses] = useState([]);
-  const [address, setAddress] = useState(defaultContractAddress);
-
-
-  const contract = useRef(null);
+  useEffect(() => {
+    getContractAddresses().then((addresses) => {
+      setContractsList(addresses)
+      if (!contractAddress) setContractAddress(addresses[0].address);
+    });
+  }, []);
 
   useEffect(() => {
     activate(ConfiguredInjectedConnector);
   }, [chainId]);
 
   useEffect(() => {
-    if (!account) return;
-    (async () => {
-      setAddresses(Object.entries(await getAdresses()))
-      contract.current = createBridgeContract(address, library.getSigner());
-    })()
-  }, [account, chainId, address]);
+    if (!account || !contractAddress) return;
+    setContract(createBridgeContract(contractAddress, library.getSigner()));
+  }, [account, chainId, contractAddress]);
+
+  const selectContract = ({target}) => {
+    setContractAddress(target.value);
+    queryParams.set('contract', target.value);
+    window.history.replaceState(undefined, undefined, queryParams.toString());
+  }
 
   return (<div>
-    <span>ChainID: {chainId}</span>
+    <span>Metamask ChainID: {chainId}</span>
     <br/>
 
     <span>Contract address: </span>
-    <input name="address" type="text" onChange={({target}) => setAddress(target.value)} value={address}
-           placeholder="Contract address"/>
-    <select onChange={(e) => setAddress(e.target.value)}>
-      {addresses.map(([k, v]) => (
-        <option value={v} key={k}>{k}</option>
+    <input name="address" type="text" onChange={selectContract} value={contractAddress} placeholder="Contract address"/>
+    <select onChange={selectContract}>
+      {contractsList.map(({stage, pair, network, address}) => (
+        <option key={address} value={address} selected={address === contractAddress}>
+          {`amb-${pair} in ${network.toUpperCase()} ${stage.toUpperCase()}net`}
+        </option>
       ))}
     </select>
 
     <br/>
-    {contract.current && <Multisig contract={contract.current}/>}
+    {contract && <Multisig contract={contract}/>}
   </div>);
 
 }
